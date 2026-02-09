4a6e5d0049c42e1085668e9d0bd0973f69ec5359
Amal Abduraimov <amal.abduraimov@mail.ru>
2026-02-09 20:28:27 +0500

    update

---
+++ js/blog.js
@@ -0,0 +1,253 @@
+const newsData = {
+  all: `
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news1.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новый маршрут: доставка в малые города
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Мы открыли новое направление для доставки в малые населённые пункты.
+          Теперь ваши грузы доберутся ещё быстрее.
+        </p>
+        <p class="text-[#3A3A3A99] space3">28 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news2.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Снижение сроков городской доставки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Благодаря оптимизации логистики мы сократили среднее время доставки
+          по городу до 3 часов.
+        </p>
+        <p class="text-[#3A3A3A99] space3">19 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news3.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новые стандарты упаковки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Ввели усиленные правила упаковки грузов для повышения сохранности
+          при перевозке.
+        </p>
+        <p class="text-[#3A3A3A99] space3">5 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news4.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Мы теперь в Telegram
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Подписывайтесь на наш Telegram-канал, чтобы первыми узнавать об
+          акциях, изменениях и новостях компании.
+        </p>
+        <p class="text-[#3A3A3A99] space3">1 июля 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news1.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новый маршрут: доставка в малые города
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Мы открыли новое направление для доставки в малые населённые пункты.
+          Теперь ваши грузы доберутся ещё быстрее.
+        </p>
+        <p class="text-[#3A3A3A99] space3">28 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news2.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Снижение сроков городской доставки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Благодаря оптимизации логистики мы сократили среднее время доставки
+          по городу до 3 часов.
+        </p>
+        <p class="text-[#3A3A3A99] space3">19 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news3.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новые стандарты упаковки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Ввели усиленные правила упаковки грузов для повышения сохранности
+          при перевозке.
+        </p>
+        <p class="text-[#3A3A3A99] space3">5 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news4.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Мы теперь в Telegram
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Подписывайтесь на наш Telegram-канал, чтобы первыми узнавать об
+          акциях, изменениях и новостях компании.
+        </p>
+        <p class="text-[#3A3A3A99] space3">1 июля 2025</p>
+      </a>
+    `,
+  company: `
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news4.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Мы теперь в Telegram
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Подписывайтесь на наш Telegram-канал, чтобы первыми узнавать об
+          акциях, изменениях и новостях компании.
+        </p>
+        <p class="text-[#3A3A3A99] space3">1 июля 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news3.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новые стандарты упаковки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Ввели усиленные правила упаковки грузов для повышения сохранности
+          при перевозке.
+        </p>
+        <p class="text-[#3A3A3A99] space3">5 июня 2025</p>
+      </a>
+      <a
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news2.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Снижение сроков городской доставки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Благодаря оптимизации логистики мы сократили среднее время доставки
+          по городу до 3 часов.
+        </p>
+        <p class="text-[#3A3A3A99] space3">19 июня 2025</p>
+      </a>
+    `,
+  delivery: `
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news1.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новый маршрут: доставка в малые города
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Мы открыли новое направление для доставки в малые населённые пункты.
+          Теперь ваши грузы доберутся ещё быстрее.
+        </p>
+        <p class="text-[#3A3A3A99] space3">28 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news2.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Снижение сроков городской доставки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Благодаря оптимизации логистики мы сократили среднее время доставки
+          по городу до 3 часов.
+        </p>
+        <p class="text-[#3A3A3A99] space3">19 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news1.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новый маршрут: доставка в малые города
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Мы открыли новое направление для доставки в малые населённые пункты.
+          Теперь ваши грузы доберутся ещё быстрее.
+        </p>
+        <p class="text-[#3A3A3A99] space3">28 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 min-[1440px]:flex-1 shrink-0 max-[768px]:w-full max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news3.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Новые стандарты упаковки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Ввели усиленные правила упаковки грузов для повышения сохранности
+          при перевозке.
+        </p>
+        <p class="text-[#3A3A3A99] space3">5 июня 2025</p>
+      </a>
+      
+        <a  href="/pages/blog.html"
+        class="max-[1440px]:w-74.5 max-[1024px]:w-88 max-[768px]:w-full min-[1440px]:flex-1 shrink-0 max-[768px]:rounded-3xl"
+      >
+        <img src="../assets/images/news2.png" class="mb-4" />
+        <p class="text-[#3A3A3A] font-medium text-2xl space3 leading-8 mb-2">
+          Снижение сроков городской доставки
+        </p>
+        <p class="text-[#3A3A3A] text-lg leading-6 space3 mb-6">
+          Благодаря оптимизации логистики мы сократили среднее время доставки
+          по городу до 3 часов.
+        </p>
+        <p class="text-[#3A3A3A99] space3">19 июня 2025</p>
+      </a>
+    `,
+};
+
+const newsButtons = document.querySelectorAll("[data-news-tab]");
+const newsContainer = document.getElementById("news-container");
+
+// Инициализация
+newsContainer.innerHTML = newsData.all;
+
+newsButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    newsButtons.forEach((b) => {
+      b.classList.remove("active", "bg-[#3A3A3A]", "text-white");
+      b.classList.add("bg-[#F2F5F7]", "text-[#3A3A3A]");
+    });
+
+    btn.classList.add("active", "bg-[#3A3A3A]", "text-white");
+    btn.classList.remove("bg-[#F2F5F7]", "text-[#3A3A3A]");
+
+    const tab = btn.getAttribute("data-news-tab");
+    newsContainer.innerHTML = newsData[tab];
+  });
+});
